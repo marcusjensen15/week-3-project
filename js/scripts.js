af10a8978837cf41cf38cd6ea9a6bbac859b9603
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-01 11:50:58 -0700

    Adding name divisible by three functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 //Business Logic
 var outputArray = [];
+var name;    //this is new
+
+function nameCheck(userInput){            //This function is new
+  if(userInput % 3 === 0){
+    name = userName;
+  }else {
+    name = "Dave";
+  }
+
+};
 
 function beepBoop(userInput){
   for (i=0; i <= userInput; i++){
     if(i.toString().includes("3")){
-      outputArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+      outputArray.push(" I'm sorry,"+ name +". I'm afraid I can't do that.");
     }else if (i.toString().includes("2")){
       outputArray.push(" Boop");
     }else if (i.toString().includes("1")){
@@ -20,11 +30,15 @@ $(document).ready(function(){
     $("#output-container").hide();
     event.preventDefault();
     outputArray = [];
+    name= "";                      //this is new
     var userInput = parseInt($("#userInput").val());
+    var userName = $("#userNameInput").val();  //this is new
+
     beepBoop(userInput);
     $("#output").text(outputArray);
     $("#output-container").slideDown("slow");
     $('#userInput').val('');
+    $('#userNameInput').val('');    //this is new
 
 
   });
