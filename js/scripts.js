ed370d5bd56be734183d452a7173da1fca14a7f4
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-01 12:58:53 -0700

    Name scoping issue fix. App now works with name input divisible by three functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 //Business Logic
 var outputArray = [];
-var userName; //this is new
-var userInput; //this is new
-var nameResult = nameCheck(userInput); //this is new I think this is calling the function right away
+var userName;
+var userInput;
+var nameResult;
 
 function nameCheck(input){
-                                  //This function is new
+
   if(input % 3 === 0){
     var name = userName;
     console.log(name);
   }else {
-    name = "Dave";
+    name = " Dave";
     console.log(name);
   }
   return name;
 
 };
-
 function beepBoop(userInput){
   for (i=0; i <= userInput; i++){
     if(i.toString().includes("3")){
@@ -30,26 +29,22 @@ function beepBoop(userInput){
   };
 };
 
-
 //User Logic
 $(document).ready(function(){
   $("form").submit(function(event){
+     event.preventDefault();
+     outputArray = [];
      userName = $('#userNameInput').val();  //this is new
+     userInput = parseInt($("#userInput").val());
+     nameResult = nameCheck(userInput);
     $("#output-container").hide();
-    event.preventDefault();
-    outputArray = [];
-    name= "";
-    userName =  $("#userNameInput").val();                    //this is new
-    userInput = parseInt($("#userInput").val());  //changed from: var userInput = parseInt($("#userInput").val());
-    // var userName = $("#userNameInput").val();  //this is new
-    nameCheck(userInput);                        //calling new function
+
     beepBoop(userInput);
     $("#output").text(outputArray);
     $("#output-container").slideDown("slow");
     $('#userInput').val('');
-    // $('#userNameInput').val('');    //this is new
+    $('#userNameInput').val('');
 
 
   });
-
 });
