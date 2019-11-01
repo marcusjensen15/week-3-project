78a4b2ff85008935210da28305ab9d13ef14504b
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-01 10:42:54 -0700

    Core functionality met. Need to add styling.

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ var outputArray = [];
 function beepBoop(userInput){
   for (i=0; i <= userInput; i++){
     if(i.toString().includes("1")){
-      outputArray.push("Beep");
+      outputArray.push(" Beep");
     }else if (i.toString().includes("2")){
-      outputArray.push("Boop");
+      outputArray.push(" Boop");
     }else if (i.toString().includes("3")){
-      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      outputArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
      }
     else {outputArray.push(i);}
   };
@@ -18,10 +18,13 @@ function beepBoop(userInput){
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    outputArray = [];
+
     var userInput = parseInt($("#userInput").val());
-    
+
     beepBoop(userInput);
-    console.log(outputArray);
+    $("#output").text(outputArray);
+  
     $('#userInput').val('');
 
 
