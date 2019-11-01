c5b621bef08bb4f740df34658f39e7f1a4d71fee
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-01 11:27:38 -0700

    add slide down effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,6 @@
 //Business Logic
 var outputArray = [];
 
-// function beepBoop(userInput){
-//   for (i=0; i <= userInput; i++){
-//     if(i.toString().includes("1")){
-//       outputArray.push(" Beep");
-//     }else if (i.toString().includes("2")){
-//       outputArray.push(" Boop");
-//     }else if (i.toString().includes("3")){
-//       outputArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
-//      }
-//     else {outputArray.push(i);}
-//   };
-// };
-
-//Everything above this line works, refactoring to order least to most important
-
 function beepBoop(userInput){
   for (i=0; i <= userInput; i++){
     if(i.toString().includes("3")){
@@ -32,14 +17,13 @@ function beepBoop(userInput){
 //User Logic
 $(document).ready(function(){
   $("form").submit(function(event){
+    $("#output-container").hide();
     event.preventDefault();
     outputArray = [];
-
     var userInput = parseInt($("#userInput").val());
-
     beepBoop(userInput);
     $("#output").text(outputArray);
-
+    $("#output-container").slideDown("slow");
     $('#userInput').val('');
 
 
