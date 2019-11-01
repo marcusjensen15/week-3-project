15b7c58d9170a871d5d0cc01b9133786dbbf5202
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-01 11:15:58 -0700

    modify if/if else statments to match with least-most important requirements

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
 //Business Logic
 var outputArray = [];
 
+// function beepBoop(userInput){
+//   for (i=0; i <= userInput; i++){
+//     if(i.toString().includes("1")){
+//       outputArray.push(" Beep");
+//     }else if (i.toString().includes("2")){
+//       outputArray.push(" Boop");
+//     }else if (i.toString().includes("3")){
+//       outputArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+//      }
+//     else {outputArray.push(i);}
+//   };
+// };
+
+//Everything above this line works, refactoring to order least to most important
+
 function beepBoop(userInput){
   for (i=0; i <= userInput; i++){
-    if(i.toString().includes("1")){
-      outputArray.push(" Beep");
+    if(i.toString().includes("3")){
+      outputArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
     }else if (i.toString().includes("2")){
       outputArray.push(" Boop");
-    }else if (i.toString().includes("3")){
-      outputArray.push(" I'm sorry, Dave. I'm afraid I can't do that.");
+    }else if (i.toString().includes("1")){
+      outputArray.push(" Beep");
      }
     else {outputArray.push(i);}
   };
@@ -24,7 +39,7 @@ $(document).ready(function(){
 
     beepBoop(userInput);
     $("#output").text(outputArray);
-  
+
     $('#userInput').val('');
 
 
