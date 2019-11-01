88fb401c9d94778ae6bee5ad7f2cb3dd8b440dd0
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-01 09:00:47 -0700

    JS functional. Console logging correct output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
-function beepBoop(){
+//Business Logic
+var outputArray = [];
+
+function beepBoop(userInput){
   for (i=0; i <= userInput; i++){
     if(i === 1){
-      outputArray.push("beep");
+      outputArray.push("Beep");
 
-    }else if 
+    }else if (i === 2){
+      outputArray.push("Boop");
+
+    }else if (i === 3){
+      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+
+    }else if (i===13){
+      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
 
 
-  }
+    }else if (i === 21){
+      outputArray.push("Boop");
+
+
+    }else if (i === 32){
+      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+
+
+    }else {outputArray.push(i);}
+  };
 
 
 
 };
 
+beepBoop(40);
+console.log(outputArray);
 
 
 
-
-
+//User Logic
 $(document).ready(function(){
-  var outputArray = [];
+  // var outputArray = [];
   var userInput = 5; //will need to parseInt userInput
-
-
-
-
-
 
 });
