4bec01bfee6f59b2efcf945510b2be69ff45b204
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-01 12:14:57 -0700

    working on getting name adjustment to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
 //Business Logic
 var outputArray = [];
-var name;    //this is new
+var userName; //this is new
+var userInput; //this is new
+var nameResult = nameCheck(userInput); //this is new
 
-function nameCheck(userInput){            //This function is new
-  if(userInput % 3 === 0){
+function nameCheck(input){            //This function is new
+  if(input % 3 === 0){
     name = userName;
   }else {
     name = "Dave";
   }
+  return name;
 
 };
 
 function beepBoop(userInput){
   for (i=0; i <= userInput; i++){
     if(i.toString().includes("3")){
-      outputArray.push(" I'm sorry,"+ name +". I'm afraid I can't do that.");
+      outputArray.push(" I'm sorry,"+ nameResult +". I'm afraid I can't do that.");
     }else if (i.toString().includes("2")){
       outputArray.push(" Boop");
     }else if (i.toString().includes("1")){
@@ -24,16 +27,18 @@ function beepBoop(userInput){
   };
 };
 
+
 //User Logic
 $(document).ready(function(){
   $("form").submit(function(event){
     $("#output-container").hide();
     event.preventDefault();
     outputArray = [];
-    name= "";                      //this is new
-    var userInput = parseInt($("#userInput").val());
-    var userName = $("#userNameInput").val();  //this is new
-
+    name= "";
+    userName=  $("#userNameInput").val();                    //this is new
+    userInput = parseInt($("#userInput").val());  //changed from: var userInput = parseInt($("#userInput").val());
+    // var userName = $("#userNameInput").val();  //this is new
+    nameCheck(userInput);                        //calling new function
     beepBoop(userInput);
     $("#output").text(outputArray);
     $("#output-container").slideDown("slow");
