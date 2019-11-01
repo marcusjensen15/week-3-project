d55f4421ec47520d31ecfa8d4515dbf5dd1a0d9a
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-01 08:39:02 -0700

    Add basic js framework. Lay out project.

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function beepBoop(){
+  for (i=0; i <= userInput; i++){
+    if(i === 1){
+      outputArray.push("beep");
+
+    }else if 
+
+
+  }
+
+
+
+};
+
+
+
+
+
+
+$(document).ready(function(){
+  var outputArray = [];
+  var userInput = 5; //will need to parseInt userInput
+
+
+
+
+
+
+});
